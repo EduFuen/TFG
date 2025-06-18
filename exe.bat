9e5e90693f461b277daa867d5f541b0f853c408e
edufuen <eduardo.fuentes@estudiantes.uva.es>
2025-06-18 15:47:20 +0200

    cambio archivo exe.bsh

--- exe.bat
+++ exe.bat
@@ -1,2 +1,2 @@
 @echo off
-java --module-path "C:\javafx\javafx-sdk-17.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar "C:\Users\efuentesa\Desktop\otros\TFG\ComproOro\ComproOro\COro.exe"
+java --module-path ".\javafx\javafx-sdk-17.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar ".\COro.exe"
